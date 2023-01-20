aea1f8dea2157669d6a38e64a5563802a66f731f
Joseph Ferano <joseph@ferano.io>
2023-01-20 19:16:24 +0700

    Pyxel api ref as org-mode file

--- flappydogs.py
+++ flappydogs.py
@@ -1,1 +1,13 @@
 import pyxel
+
+class App:
+    def __init__(self):
+        pyxel.init(320,280, title="Flappy Dogs")
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+
+    def draw(self):
+        pyxel.cls(12)
