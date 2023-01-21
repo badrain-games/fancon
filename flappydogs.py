1fdc3864e524312e7f488e22816ce163e31247c3
Joseph Ferano <joseph@ferano.io>
2023-01-21 23:30:05 +0700

    Added forks

--- flappydogs.py
+++ flappydogs.py
@@ -1,17 +1,20 @@
 import pyxel
 
-playerX = 60
-jumpBoost = 2.6
+player_x = 60
+jump_boost = 2.6
 gravity = 0.228
+fork_separation = 60
+fork_speed = 2
 
 class App:
     def __init__(self):
         pyxel.init(250,160, title="Flappy Dogs")
         pyxel.load("Assets/flappydogs.pyxres")
 
-        self.playerY = 30
-        self.playerDy = jumpBoost
-        self.isDead = False
+        self.player_y = 30
+        self.player_dy = jump_boost
+        self.is_dead = False
+        self.forks = [(i * fork_separation + pyxel.width, 30) for i in range(1, 6)]
 
         pyxel.run(self.update, self.draw)
 
@@ -20,19 +23,25 @@ class App:
             pyxel.quit()
 
         if pyxel.btn(pyxel.KEY_SPACE):
-            self.playerDy = jumpBoost
+            self.player_dy = jump_boost
 
-        self.playerDy -= gravity
-        self.playerY -= self.playerDy
+        self.player_dy -= gravity
+        self.player_y -= self.player_dy
 
-        if self.playerY > pyxel.height:
-            self.isDead = True
+        if self.player_y > pyxel.height:
+            self.is_dead = True
+
+        for i,(x,y) in enumerate(self.forks):
+            self.forks[i] = (x - fork_speed, y)
 
 
     def draw(self):
         pyxel.cls(12)
-        pyxel.blt(playerX, self.playerY, 0, 0, 32, 16, 16, 0)
+        pyxel.blt(player_x, self.player_y, 0, 0, 32, 16, 16, 0)
+        # pyxel.blt(playerX + 20, 0, 0, 0, 48, 16, 32, 0)
+        for fork in self.forks:
+            pyxel.blt(fork[0], fork[1], 0, 0, 80, 16, 16, 0)
         pyxel.text(5, 5, "Flappy Dogs", 1)
-        pyxel.text(5, 10, f"{self.playerY}", 1)
+        pyxel.text(5, 10, f"{self.player_y}", 1)
 
 App()
