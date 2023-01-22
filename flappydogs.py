8a7da58fe97da3991c58ce620380604cda9e6773
Joseph Ferano <joseph@ferano.io>
2023-01-22 22:56:32 +0700

    Add fork handles

--- flappydogs.py
+++ flappydogs.py
@@ -1,4 +1,5 @@
 import pyxel
+import random
 
 player_x = 60
 jump_boost = 1.93
@@ -8,15 +9,18 @@ fork_spawn_interval = 50
 fork_spawn_count = 8
 fork_midgap = 50
 
+def get_random_height():
+    return random.randrange(20,90)
+
 class App:
     def __init__(self):
-        pyxel.init(250,160, title="Flappy Dogs", fps=60)
+        pyxel.init(160,160, title="Flappy Dogs", fps=60)
         pyxel.load("Assets/flappydogs.pyxres")
 
         self.player_y = 30
         self.player_dy = jump_boost
         self.is_dead = False
-        self.forks = [(16 + pyxel.width, 30) for i in range(8)]
+        self.forks = [(-16, get_random_height()) for i in range(8)]
         self.fork_spawn_idx = 0
 
         pyxel.run(self.update, self.draw)
@@ -44,15 +48,20 @@ class App:
             x_ = x - fork_speed
             if fork_spawn_reset == i:
                 x_ = pyxel.width + 16
+                y = get_random_height()
             self.forks[i] = (x_, y)
 
 
     def draw(self):
         pyxel.cls(12)
-        # pyxel.blt(playerX + 20, 0, 0, 0, 48, 16, 32, 0)
         for fork_x,fork_y in self.forks:
+            for i in range(6):
+                pyxel.blt(fork_x, fork_y - (16 * (i + 1)), 0, 0, 64, 16, 16, 0)
             pyxel.blt(fork_x, fork_y, 0, 0, 80, 16, 16, 0)
+
             pyxel.blt(fork_x, fork_y + fork_midgap, 0, 0, 48, 16, 16, 0)
+            for i in range(6):
+                pyxel.blt(fork_x, fork_y + fork_midgap + (16 * (i + 1)), 0, 0, 64, 16, 16, 0)
         pyxel.blt(player_x, self.player_y, 0, 0, 32, 16, 16, 0)
         pyxel.text(5, 5, "Flappy Dogs", 1)
 
