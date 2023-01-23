e26f69cbc5c522653a75a6136b23222f4eeae8d8
Joseph Ferano <joseph@ferano.io>
2023-01-23 19:22:41 +0700

    Added collision detection and debugging flag

--- flappydogs.py
+++ flappydogs.py
@@ -2,6 +2,7 @@ import pyxel
 import random
 
 player_x = 60
+sprite_size = 16
 jump_boost = 1.93
 gravity = 0.139
 fork_speed = 1
@@ -19,9 +20,10 @@ class App:
 
         self.player_y = 30
         self.player_dy = jump_boost
-        self.is_dead = False
-        self.forks = [(-16, get_random_height()) for i in range(8)]
+        self.death_count = 0
+        self.forks = [(-sprite_size, get_random_height()) for i in range(8)]
         self.fork_spawn_idx = 0
+        self.debug = True
 
         pyxel.run(self.update, self.draw)
 
@@ -47,24 +49,36 @@ class App:
         for i,(x,y) in enumerate(self.forks):
             x_ = x - fork_speed
             if fork_spawn_reset == i:
-                x_ = pyxel.width + 16
+                x_ = pyxel.width + sprite_size
                 y = get_random_height()
             self.forks[i] = (x_, y)
+
+        for (x,y) in self.forks:
+            pright = player_x + sprite_size - 2
+            player_height = sprite_size // 2
+            if (((player_x + 2 > x and player_x + 2 < x + sprite_size)
+                or (pright > x and pright < x + sprite_size))
+                and (self.player_y + 5 < y + sprite_size or
+                     self.player_y + sprite_size - 5 > y + fork_midgap)):
+                self.death_count += 1
 
 
     def draw(self):
         pyxel.cls(12)
-        pyxel.blt(0, 0, 0, 16, 0, 32, 32)
+        pyxel.blt(0, 0, 0, sprite_size, 0, 32, 32)
         for fork_x,fork_y in self.forks:
             for i in range(6):
-                pyxel.blt(fork_x, fork_y - (16 * (i + 1)), 0, 0, 64, 16, 16, 0)
-            pyxel.blt(fork_x, fork_y, 0, 0, 80, 16, 16, 0)
+                pyxel.blt(fork_x, fork_y - (sprite_size * (i + 1)), 0, 0, 64, sprite_size, sprite_size, 0)
+            pyxel.blt(fork_x, fork_y, 0, 0, 80, sprite_size, sprite_size, 0)
 
-            pyxel.blt(fork_x, fork_y + fork_midgap, 0, 0, 48, 16, 16, 0)
+            pyxel.blt(fork_x, fork_y + fork_midgap, 0, 0, 48, sprite_size, sprite_size, 0)
             for i in range(6):
-                pyxel.blt(fork_x, fork_y + fork_midgap + (16 * (i + 1)), 0, 0, 64, 16, 16, 0)
-        u = 16 if self.player_dy > 0 else 0
-        pyxel.blt(player_x, self.player_y, 0, u, 32, 16, 16, 0)
+                pyxel.blt(fork_x, fork_y + fork_midgap + (sprite_size * (i + 1)), 0, 0, 64, sprite_size, sprite_size, 0)
+        u = sprite_size if self.player_dy > 0 else 0
+        pyxel.blt(player_x, self.player_y, 0, u, 32, sprite_size, sprite_size, 0)
         pyxel.text(5, 5, "Flappy Dogs", 1)
+        if self.debug:
+            pyxel.text(5, 15, str(self.player_y), 1)
+            pyxel.text(5, 25, f"Dead Count: {self.death_count}", 8)
 
 App()
