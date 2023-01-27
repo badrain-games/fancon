c987fa2aae1a6759cf259ab8301e1770e52d255e
Joseph Ferano <joseph@ferano.io>
2023-01-27 21:19:55 +0700

    Sploosh animation

--- flappydogs.py
+++ flappydogs.py
@@ -7,6 +7,7 @@ jump_boost = 1.93
 gravity = 0.139
 fork_speed = 1
 tree_speed = 1
+sploosh_speed = 0.5
 fork_spawn_interval = 70
 fork_spawn_count = 8
 tree_spawn_count = 6
@@ -26,6 +27,7 @@ class App:
         self.forks = [(-sprite_size, get_random_height(), True) for i in range(fork_spawn_count)]
         self.fork_spawn_idx = 0
         self.trees = [(i * 32, pyxel.height - 32) for i in range(tree_spawn_count)]
+        self.sploosh_anims = []
         self.debug = False
 
         pyxel.run(self.update, self.draw)
@@ -47,6 +49,9 @@ class App:
 
         if pyxel.btn(pyxel.KEY_SPACE):
             self.player_dy = jump_boost
+
+        if pyxel.btnp(pyxel.KEY_SPACE):
+            self.sploosh_anims.append((player_x, self.player_y, pyxel.frame_count))
 
         self.player_dy -= gravity
         self.player_y -= self.player_dy
@@ -94,6 +99,8 @@ class App:
 
                 self.trees[i] = (x_, y)
 
+        for i,(x,y,fc) in enumerate(self.sploosh_anims):
+            self.sploosh_anims[i] = x - sploosh_speed,y,fc
 
 
     def draw(self):
@@ -114,6 +121,14 @@ class App:
             for i in range(6):
                 pyxel.blt(fork_x, fork_y + fork_midgap + (sprite_size * (i + 1)), 0, 0, 64, sprite_size, sprite_size, 0)
 
+        anim_speed = 5
+        # Draw hotdog animation
+        for x,y,fc in self.sploosh_anims:
+            animx = ((pyxel.frame_count - fc) // anim_speed) * sprite_size
+            pyxel.blt(x, y, 0, animx, 96, sprite_size, sprite_size, 0)
+
+        ff = lambda sploosh: ((pyxel.frame_count - sploosh[2]) // anim_speed) < 4
+        self.sploosh_anims = list(filter(ff, self.sploosh_anims))
 
 
         u = sprite_size if self.player_dy > 0 else 0
@@ -121,6 +136,6 @@ class App:
         pyxel.text(5, 5, f"Score {self.score}", 1)
         if self.debug:
             pyxel.text(5, 15, str(self.player_y), 1)
-            pyxel.text(5, 25, f"Dead Count: {self.death_count}", 8)
+            pyxel.text(5, 35, f"Anims {len(self.sploosh_anims)}", 8)
 
 App()
