7ca9a54ebc6af9eda3e6d9409942b88483170220
Joseph Ferano <joseph@ferano.io>
2023-01-31 12:58:34 +0700

    Depth First Search (DFS) now works for pathfinding

--- pathfinding.py
+++ pathfinding.py
@@ -5,12 +5,19 @@ from lib import RectPos
 from dataclasses import dataclass
 
 @dataclass
+class Node:
+    index: (int,int)
+    edges: list
+    visited: bool
+
+@dataclass
 class World:
     player_pos = (0,0)
     player_dir = 0
     player_state = "Idle"
     mouse_click = None
     surrounding_grid = [(0,0)] * 8
+    path = []
     angle = 0
 
 player_speed = 1.2
@@ -30,42 +37,53 @@ def player_rect():
     return (*world.player_pos, 8, 8)
 
 def generate_graph():
-    graph = [list() for _ in range(16 * 16)]
+    graph = [None] * (16 * 16)
     for row in range(16):
         for col in range(16):
             if not is_boundary(col * 8, row * 8):
-                tiles = lib.get_surrounding_tiles(0, col*8+1, row*8+1)
+                tiles = lib.get_surrounding_tiles(0, col*8, row*8)
                 filtered = filter(lambda xyt: xyt[2] == 1, tiles)
                 edges = list(map(lambda xyt: (xyt[1]//8,xyt[0]//8), filtered))
-                graph[row * 16 + col] = (edges,False)
+                graph[row * 16 + col] = Node((row,col), edges, False)
     return graph
 
-def find_path(start_node, target_node):
-    visited = start_node[1] 
-    if start_node == target_node:
+def debug_print_graph():
+    for i,node in enumerate(map_graph):
+        if node:
+            print(f"Tile {node.index}:")
+            for edge in node.edges:
+                print("\t", edge)
+
+def get_tile_coord(x,y):
+    return y//8,x//8
+
+def get_node_at(row,col):
+    idx = int(row * 16 + col)
+    return map_graph[idx]
+
+def set_node_at(row,col,val):
+    map_graph[int(col // 8 * 16 + col // 8 % 16)]
+
+def dfs(current_node, target_node):
+    if current_node == target_node:
         return [target_node]
-    elif visited:
+    elif current_node.visited:
         return []
     else:
-        for n in start_node[0]:
-            end_node = find_path(n, target_node)
+        current_node.visited = True
+        for n in current_node.edges:
+            nn = get_node_at(*n)
+            end_node = dfs(nn, target_node)
             if end_node:
-                return [start_node] + end_node
-
-def debug_print_nodes():
-    for i,node in enumerate(map_graph):
-        if node and node[0]:
-            print(f"Tile at {i // 16+1},{(i % 16)+1}:")
-            for edge in node[0]:
-                print("\t", f"({edge[0]+1},{edge[1]+1})")
+                return [current_node] + end_node
 
 def init():
     pyxel.init(128,128, title="Pathfinding", fps=60, display_scale=4)
     pyxel.load("Assets/pathfinding.pyxres")
 
-    global map_graph 
-
+    global map_graph
     map_graph = generate_graph()
+    # debug_print_graph()
     world.player_pos = pyxel.width // 2 - 12, pyxel.height // 2 - 12
     pyxel.run(update, draw)
 
@@ -111,14 +129,18 @@ def update():
     for pt in rect_points:
         px,py = check_boundary(px, py, pt)
 
+    global map_graph
     if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
         world.mouse_click = pyxel.mouse_x, pyxel.mouse_y
 
-        player_node = map_graph[int(py // 8 * 16 + px // 8 % 16)]
-        target_node = map_graph[pyxel.mouse_y // 8 * 16 + (pyxel.mouse_x // 8 % 16)]
-        p = find_path(player_node, target_node)
-        print(p)
-                  
+        player_node = get_node_at(*get_tile_coord(px,py))
+        target_node = get_node_at(*get_tile_coord(pyxel.mouse_x,pyxel.mouse_y))
+        for node in map_graph:
+            if node:
+                node.visited = False
+
+        p = dfs(player_node, target_node)
+        world.path = p
 
 
     world.player_pos = px,py
@@ -141,9 +163,13 @@ def draw():
     # pcx,pcy = lib.rect_point(RectPos.Center, (px,py,8,8))
     # mark_tiles(lib.get_surrounding_tiles(0,pcx,pcy))
 
-    if world.mouse_click is not None:
-        mx,my = world.mouse_click
-        mark_tiles(lib.get_surrounding_tiles(0,mx,my))
+    for n in world.path:
+        pyxel.rect(n.index[1]*8+3, n.index[0]*8+3, 2, 2, 7)
+
+
+    # if world.mouse_click is not None:
+    #     mx,my = world.mouse_click
+    #     mark_tiles(lib.get_surrounding_tiles(0,mx,my))
 
     # tx,ty = pyxel.tilemap(0).pget(pyxel.mouse_x // 8, pyxel.mouse_y // 8)
     # pyxel.text(5,3, f"Tile at {pyxel.mouse_y // 8+1}, {pyxel.mouse_x // 8+1}: {tx},{ty}", 7)
